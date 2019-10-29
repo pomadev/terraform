0edeef1a138b8f5eb1bccc536a7e82a72a74be6a
pomadev <y.pomadev@gmail.com>
2019-10-29 23:45:36 +0900

    パブリックサブネットのマルチAZ化

--- network.tf
+++ network.tf
@@ -9,11 +9,18 @@ resource "aws_vpc" "example" {
   }
 }
 
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public_0" {
   vpc_id  = aws_vpc.example.id
-  cidr_block = "10.0.0.0/24"
-  map_public_ip_on_launch = true
+  cidr_block = "10.0.1.0/24"
   availability_zone = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id  = aws_vpc.example.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-northeast-1c"
+  map_public_ip_on_launch = true
 }
 
 // インターネットゲートウェイ
